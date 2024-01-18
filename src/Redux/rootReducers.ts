f2535c0309385b88af1f5cfdd8511175210a0a85
WHITEBEARD\nisha <nishantsahai221@gmail.com>
2024-01-18 22:10:42 +0530

    removed commented code

--- src/Redux/rootReducers.ts
+++ src/Redux/rootReducers.ts
@@ -1,6 +1,4 @@
 import {combineReducers} from '@reduxjs/toolkit';
-// Import your reducers here
-// import exampleReducer from './exampleReducer';
 import counterSlice from './Slices/counterSlice';
 
 const rootReducer = combineReducers({

6ec08a77eab2186e527812a54a2fb1d92332d907
WHITEBEARD\nisha <nishantsahai221@gmail.com>
2024-07-15 22:09:50 +0530

    changes baseUrl to point to deployed server

--- src/Constants/index.ts
+++ src/Constants/index.ts
@@ -29,7 +29,7 @@ const allGenres = [
     "Traveling",
   ];
 
-  const baseURL = 'http://localhost:8001'
-  // const baseURL = "https://justask-backend.onrender.com"
+  // const baseURL = 'http://localhost:8001'
+  const baseURL = "https://justask-backend.onrender.com"
 
   export {allGenres, baseURL}
